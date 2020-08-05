ce5b9a14a3265d7f0e80971365778a0e21522e18
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 11:11:04 +0300

    init project

--- HowToCreateASwiftUITabBar/ContentView.swift
+++ HowToCreateASwiftUITabBar/ContentView.swift
@@ -9,8 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Home View")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            Text("Account View")
+                .tabItem {
+                    Image(systemName: "person.circle")
+                    Text("Account")
+                }
+            
+            Text("Friends View")
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Friends")
+                }
+            
+            Text("Add friend View")
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill.badge.plus")
+                    Text("Add")
+                }
+            
+            Text("Setting View")
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+            
+            Text("History View")
+                .tabItem {
+                    Image(systemName: "rectangle.stack.fill.badge.person.crop")
+                    Text("History")
+                }
+            
+            Text("Cloud View")
+                .tabItem {
+                    Image(systemName: "person.icloud")
+                    Text("Cloud")
+                }
+        }
     }
 }
 
